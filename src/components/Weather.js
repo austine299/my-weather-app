b7f7c7af1514ae3fac2e2882be2de9a155a4c569
austine <imahaugustine@gmail.com>
2025-03-11 16:19:08 +0100

    update

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -7,7 +7,7 @@ const WeatherApp = () => {
   const [error, setError] = useState(null);
 
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=ec095b6856962a9098bccfea8db60a7c`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=ec095b6856962a9098bccfea8db60a7c`
 
   const searchLocation = (event) =>{
     if(event.key === "Enter"){
@@ -39,7 +39,7 @@ const WeatherApp = () => {
                 <p>{data.name}</p>
             </div>
             <div className="temp">
-                {data.main ? <h1>{data.main.temp}</h1>:null}
+                {data.main ? <h1>{data.main.temp.toFixed()}°F</h1>:null}
             </div>
             <div className="description">
                 {data.weather ? <p>{data.weather[0].main}</p>:null}
@@ -48,15 +48,15 @@ const WeatherApp = () => {
         {data.name != undefined && 
             <div className="bottom">
                 <div className="feels">
-                    {data.main ? <p className="bold">{data.main.feels_like}</p>:null}
+                    {data.main ? <p className="bold">{data.main.feels_like.toFixed()} °F</p>:null}
                     <p> Feels like</p>
                 </div>
                 <div className="humidity">
-                    {data.main ? <p className="bold">{data.main.humidity}</p>:null}
+                    {data.main ? <p className="bold">{data.main.humidity}%</p>:null}
                     <p>Humidity</p>
                 </div>
                 <div className="wind">
-                    {data.wind ? <p className="bold">{data.wind.speed}</p>:null}
+                    {data.wind ? <p className="bold">{data.wind.speed}MPH</p>:null}
                     <p>Wind Speed</p>
                 </div>
             </div>
